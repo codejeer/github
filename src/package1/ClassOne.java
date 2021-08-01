abe1101193be345f3935f8b27b8afe9eb16a293c
o-code-cyber <demirorhan0555@gmail.com>
2021-08-01 22:42:10 +0300

    third commit

--- src/package1/ClassOne.java
+++ src/package1/ClassOne.java
@@ -4,5 +4,6 @@ public class ClassOne {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Batch 35 36 Learn github");
+        System.out.println("We like igt  but we will like more when we have more practice");
     }
 }
